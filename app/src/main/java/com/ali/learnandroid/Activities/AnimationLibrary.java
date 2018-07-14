ddaa19e7fbf3d057752ffc519e292fdb9666edb4
ali <zarrib.ali0@gmail.com>
2018-07-14 11:00:32 +0500

    Design Changes

--- app/src/main/java/com/ali/learnandroid/Activities/AnimationLibrary.java
+++ app/src/main/java/com/ali/learnandroid/Activities/AnimationLibrary.java
@@ -2,6 +2,7 @@ package com.ali.learnandroid.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,6 +22,8 @@ public class AnimationLibrary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation_library);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         ivStep1 = findViewById(R.id.ivAnimationLib);
         btnDemo = findViewById(R.id.btnDemoAnimationsLib);
@@ -50,7 +53,20 @@ public class AnimationLibrary extends AppCompatActivity {
                         .playOn(btnDemo);
             }
         });
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
+    @Override
+    public void onBackPressed() {
+        finish();
+        super.onBackPressed();
     }
 }
